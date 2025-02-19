2afad0ff8a039f66627e4ad198ed20efad87f30e
cara80 <carinahackl8@gmail.com>
2025-02-19 09:02:25 +0100

    fertige repos

--- OuterrimSpaceship/Web_OuterrimSpaceship/Repos/IRepositoryAsync.cs
+++ OuterrimSpaceship/Web_OuterrimSpaceship/Repos/IRepositoryAsync.cs
@@ -1,6 +1,24 @@
-﻿namespace Model_OuterrimSpaceship.Repos;
+﻿using System.Linq.Expressions;
 
-public class IRepositoryAsync
+namespace Model_OuterrimSpaceship.Repos;
+
+public interface IRepositoryAsync<TEntity> where TEntity : class
 {
-    
+    Task<TEntity> CreateAsync(TEntity t);
+
+    Task<List<TEntity>> CreateRangeAsync(List<TEntity> list);
+
+    Task UpdateAsync(TEntity t);
+
+    Task UpdateRangeAsync(List<TEntity> list);
+
+    Task<TEntity?> ReadAsync(int id);
+
+    Task<List<TEntity>> ReadAsync(Expression<Func<TEntity, bool>> filter);
+
+    Task<List<TEntity>> ReadAsync(int start, int count);
+
+    Task<List<TEntity>> ReadAllAsync();
+
+    Task DeleteAsync(TEntity t);
 }
